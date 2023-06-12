892ea09df92efea4d8a8640e6ca16e3e643df488
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-06-12 09:06:24 -0400

    Some style changes for the form and images

--- app/helpers/blog_posts_helper.rb
+++ app/helpers/blog_posts_helper.rb
@@ -1,11 +1,9 @@
 module BlogPostsHelper
   def output_time_info(blog_post)
-    unless blog_post.published_at.nil?
-      if blog_post.scheduled?
-        content_tag(:p, "To be published on #{blog_post.published_at.to_fs(:long)}")
-      else
-        content_tag(:p, "#{time_ago_in_words(blog_post.published_at)} ago")
-      end
+    if blog_post.scheduled?
+      content_tag(:p, "To be published on #{blog_post.published_at.to_fs(:long)}")
+    elsif blog_post.published?
+      content_tag(:p, "#{time_ago_in_words(blog_post.published_at)} ago")
     end
   end
 end
