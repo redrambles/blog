2c4c019e93c05ae18c0421480b63ac214e09cf7b
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-06-16 09:07:52 -0400

    Error handling for invalid page parameter

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -9,6 +9,10 @@ class BlogPostsController < ApplicationController
       BlogPost.published.sorted
     end
     @pagy, @blog_posts = pagy(@blog_posts, items: 10)
+  rescue Pagy::OverflowError
+    redirect_to root_path(page: 1)
+    # params[:page] = 1
+    # retry
   end
 
   def show
